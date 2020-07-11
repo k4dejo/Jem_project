96fba5783849078495e87d245b50b46571845891
SpaceUnicorn-bit <breinersalas@gmail.com>
2020-07-11 15:58:59 -0600

    obligao

--- bambu-jem/src/app/models/imgUrl.ts
+++ bambu-jem/src/app/models/imgUrl.ts
@@ -1,5 +1,5 @@
 export let ImgUrl = {
-    //  url: 'http://bambujem.com.devel/catalogo/'
+    // url: 'http://bambujem.com.devel/catalogo/'
     url: 'https://api.modajem.com/storage/catalogo/'
     // url: 'http://35.236.74.139/storage/catalogo/'
 };
--- bambu-jem/src/app/services/global.ts
+++ bambu-jem/src/app/services/global.ts
@@ -2,4 +2,4 @@ export let GLOBAL = {
     // url: 'http://bambuJem.com.devel/api/'
     url: 'https://api.modajem.com/api/'
     // url: 'http://35.236.74.139/api/'
-}
+};
