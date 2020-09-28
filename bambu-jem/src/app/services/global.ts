43c6c56bb8ab8d99241202ba90d86fee8cf2fa10
SpaceUnicorn-bit <breinersalas14@gmail.com>
2020-09-28 10:56:34 -0600

    merge

--- bambu-jem/src/app/services/global.ts
+++ bambu-jem/src/app/services/global.ts
@@ -1,5 +1,5 @@
 export let GLOBAL = {
     // url: 'http://bambuJem.com.devel/api/'
-    // url: 'https://apiweb.modajem.com/api/'
-    url: 'http://34.94.70.181/api/'
+    url: 'https://apiweb.modajem.com/api/'
+    // url: 'http://34.94.70.181/api/'
 };
